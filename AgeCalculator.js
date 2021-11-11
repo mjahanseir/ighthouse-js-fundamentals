8be2078b4826dfad1ae5e6cfcbb9f58ba8b6c4b6
mjahanseir <mjahanseir@gmail.com>
2021-11-10 23:58:47 -0600

    last commit

---
+++ AgeCalculator.js
@@ -0,0 +1,13 @@
+
+console.log(ageCalculator("Suzie", 1983, 2015));
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
+
+
+function ageCalculator(name, yearOfBirth, currentYear) {
+  let age = currentYear - yearOfBirth;
+  let message = '"' + name + ' is ' + age + ' years old." ';
+  return message;
+
+}
