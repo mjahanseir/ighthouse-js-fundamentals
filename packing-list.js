e989fd29447d0330388f7a81949258c31c695f40
mjahanseir <mjahanseir@gmail.com>
2021-11-11 22:18:43 -0600

    Iterating Over Arrays exercise third commit

--- packing-list.js
+++ packing-list.js
@@ -6,11 +6,9 @@ for (let i = 0; i < packingList.length; i++) {
 }
 
 
-
 console.log("Kitchen stuff to pack:");
 let i = 0;
 while (i < packingList.length) {
   console.log(packingList[i]);
   i++;
 }
-
